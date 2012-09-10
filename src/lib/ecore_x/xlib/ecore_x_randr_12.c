52ab285c15166f5a6734c7bc65b5778eb537594c
Christopher Michael <cpmichael1@comcast.net>
2012-09-10 12:27:27 +0000

    Ecore_X: Backport randr crtcs fix and update ChangeLog.
    SVN revision: 76399

--- src/lib/ecore_x/xlib/ecore_x_randr_12.c
+++ src/lib/ecore_x/xlib/ecore_x_randr_12.c
@@ -527,13 +527,17 @@ ecore_x_randr_crtcs_get(Ecore_X_Window root,
    XRRScreenResources *res = NULL;
    Ecore_X_Randr_Crtc *ret = NULL;
 
-   if (num && root &&
+   if (root &&
        (res = _ecore_x_randr_get_screen_resources (_ecore_x_disp, root)))
      {
         if ((ret = malloc(sizeof(Ecore_X_Randr_Crtc) * res->ncrtc)))
           {
-             memcpy(ret, res->crtcs, (sizeof(Ecore_X_Randr_Crtc) * res->ncrtc));
-             *num = res->ncrtc;
+             int i = 0;
+
+             if (num) *num = res->ncrtc;
+
+             for (i = 0; i < res->ncrtc; i++)
+               ret[i] = res->crtcs[i];
           }
 
         XRRFreeScreenResources(res);
