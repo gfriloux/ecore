0b70a96450767e98468ce7cc1a5b3bad26500a25
Guillaume Friloux <guillaume@friloux.me>
2017-06-14 14:11:12 +0200

    [Windows] Force exit of main loop is we get invalid parameter errors.
    This error can happen when using azy client, or playing with ecore_exe.
    This results in an infinite loop inside ecore's main loop.
    This instead of locking the app and eat one thread, let just exit.

--- src/lib/ecore/ecore_main.c
+++ src/lib/ecore/ecore_main.c
@@ -2043,6 +2043,13 @@ _ecore_main_win32_select(int             nfds __UNUSED__,
      {
         char *m;
 
+        if (GetLastError() == 87)
+          {
+             CRI("Critical error inside the main loop! MsgWaitForMultipleObjects() returns invalid parameter error. This means ecore tries to monitor events on an invalid handle!");
+             ecore_main_loop_quit();
+             return -1;
+          }
+
         m = evil_last_error_get();
         ERR("%s", m);
         free(m);
